aec82bfbee8ce1f4a0935107b40bbe0476b2540b
Ryan Luu <ryanluu@Ryans-MBP-2.home>
2017-08-04 15:51:08 -0400

    More Imagery

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const express = require('express');
 const path    = require('path');
 const app     = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
